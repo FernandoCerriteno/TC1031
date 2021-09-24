798f5ebd7ab51e97a168ffe103e1880b5740168b
Fernando Cerriteño Magaña <90717713+Shedew@users.noreply.github.com>
2021-09-24 10:27:07 -0500

    Add files via upload

---
+++ Act 1.3/dateTime.h
@@ -0,0 +1,25 @@
+#ifndef DATETIME_H
+#define DATETIME_H
+
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include "Entrada.h"
+
+using namespace std;
+
+class dateTime {
+  private: 
+    vector<string> meses = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    struct tm dateStruct;
+    time_t date;
+    
+  public:
+    dateTime(string mes, int dia, int horas, int minutos, int segundos);
+    bool operator ==(const dateTime&);
+    bool operator >(const dateTime&);
+    bool operator <(const dateTime&);
+    int busquedaBinaria(vector<Entrada> lista,int size,dateTime key);
+};
+
+#endif
